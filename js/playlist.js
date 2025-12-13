f1441c99e60f895736a1087f02cd0ea72ac8da80
chenyangli1004 <leefem1004@yeah.net>
2025-12-13 22:52:31 +0000

    1

--- js/playlist.js
+++ js/playlist.js
@@ -1,47 +1,15 @@
 // *** QUAN TRỌNG: Đã cập nhật theo danh sách nhạc của bạn ***
 const playlist = [
+       {
+        title: 'Stay with me',
+        artist: 'Sam Smith',
+        coverPath: 'assets/covers/lily.jpg',
+        audioPath: 'assets/music/Stay with me.mp3'
+    },
     {
         title: 'Rumour Has It',
         artist: 'Adele',
         coverPath: 'assets/covers/rumor has it.png',
         audioPath: 'assets/music/Rumour Has It - Adele.mp3'
-    },
-
-    {
-        title: 'All Falls Down',
-        artist: 'Alan Walker',
-        coverPath: 'assets/covers/Alan_Walker_All_Falls_Down.jpg', // Lưu ý tên file ảnh
-        audioPath: 'assets/music/Alan Walker - All Falls Down.mp3'
-    },
-    {
-        title: 'Faded',
-        artist: 'Alan Walker',
-        coverPath: 'assets/covers/faded.png',
-        audioPath: 'assets/music/Alan Walker - Faded.mp3'
-    },
-    {
-        title: 'The Spectre',
-        artist: 'Alan Walker',
-        coverPath: 'assets/covers/spectre.jpg',
-        audioPath: 'assets/music/Alan Walker - The Spectre (Lyrics).mp3'
-    },
-    {
-        title: 'Lily',
-        artist: 'Alan Walker, K-391 & Emelie Hollow',
-        coverPath: 'assets/covers/lily.jpg',
-        audioPath: 'assets/music/Alan Walker, K-391 & Emelie Hollow - Lily.mp3'
-    },
-
-    {
-        title: 'Way Back Home',
-        artist: 'Conor Maynard',
-        coverPath: 'assets/covers/Way Back Home.jpg',
-        audioPath: 'assets/music/Conor Maynard - Way Back Home.mp3'
-    },
-    {
-        title: 'That Girl',
-        artist: 'Olly Murs',
-        coverPath: 'assets/covers/that girl.png',
-        audioPath: 'assets/music/That Girl - Olly Murs.mp3'
     }
 ];
